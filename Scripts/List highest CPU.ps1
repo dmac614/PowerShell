fa77d952f529269a78dad97eb0288bf273dfd4ce
dmac614 <89596346+dmac614@users.noreply.github.com>
2024-09-28 23:28:31 +1000

    Fix typos

---
+++ Scripts/List highest CPU.ps1
@@ -0,0 +1,16 @@
+﻿<#
+.SYNOPSIS
+List processes using the most CPU.
+
+.DESCRIPTION
+This script gathers the top 10 CPU processes in descending order. The custom property object divides the CPU proprty by 1KB and displays this as a percentage.
+
+.NOTES
+Author: Daniel Macdonald
+
+#>
+
+$highestCPU = get-process | Sort-Object CPU -Descending -Top 10 | Select-Object name,@{
+    n='CPU';
+    e={ '{0:P1}' -f ($_.CPU / 1KB) }
+}
