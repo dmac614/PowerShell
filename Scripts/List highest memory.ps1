8d30b2c3463efb26f81ce3b5abe2507db0a6f7c8
dmac614 <89596346+dmac614@users.noreply.github.com>
2024-09-28 23:27:51 +1000

    Create List highest memory.ps1

---
+++ Scripts/List highest memory.ps1
@@ -0,0 +1,16 @@
+﻿<#
+.SYNOPSIS
+List processes using the most memory
+
+.DESCRIPTION
+This script gathers the top 10 WorkingSet (memory) processes in descending order. The custom property object divides the memory proprty by 1GB and displays this as a percentage.
+
+.NOTES
+Author: Daniel Macdonald
+
+#>
+
+$highestRAM = get-process | Sort-Object WorkingSet -Descending -Top 10 | Select-Object name,@{
+    n='Memory';
+    e={'{0:P1}' -f ($_.WorkingSet / 1GB) }
+}
