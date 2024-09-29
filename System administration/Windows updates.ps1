eca1420ec93ca6c86cd01f344ec4bfdd043d89f1
dmac614 <89596346+dmac614@users.noreply.github.com>
2024-09-29 16:13:00 +1000

    Create Windows updates.ps1

---
+++ System administration/Windows updates.ps1
@@ -0,0 +1,20 @@
+﻿<#
+.SYNOPSIS
+Install Windows updates 
+
+.DESCRIPTION
+Install Windows updates 
+
+.NOTES
+Author: Daniel Macdonald
+
+#>
+
+# Set execution policy
+Set-ExecutionPolicy remotesigned
+
+# Install then import module
+Install-module -name pswindowsupdate | Import-Module pswindowsupdate
+
+# Get updates, accept them, then install them
+Get-WindowsUpdate -AcceptAll -Install -Verbose
