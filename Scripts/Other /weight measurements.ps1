dde8da35f3236d4b035448445a1c0bb19b7d02b3
dmac614 <89596346+dmac614@users.noreply.github.com>
2024-09-29 14:18:51 +1000

    Update weight measurements.ps1
    Fixed user input code by specifying the data types of the variables containing read-host.

--- Scripts/Other /weight measurements.ps1
+++ Scripts/Other /weight measurements.ps1
@@ -3,7 +3,7 @@
 Calculates my weight.
 
 .DESCRIPTION
-Uses a formula to calculate my weight in pounds and in stone displaying both results in two decimal places.
+Uses a formula to calculate my weight in pounds, and in stone displaying both results as two decimal numbers.
 
 .NOTES
 Author: Daniel Macdonald
@@ -12,21 +12,21 @@ Author: Daniel Macdonald
 
 function WeightMeasurements {
 # Variables
-$weight_kg = 76
 $date = get-date
-
-# Prompt the user for their name
-$Name = read-host -Prompt "What is your name?"
-
+    
+# Get user info
+[string]$Name = read-host -Prompt "What is your name?"
+[int]$weight_kg = read-host -Prompt "What is your weight?"
+    
 # Convert kilograms to pounds: kg * 2.2045 = lbs
 $GetPounds = '{0:n2}' -f ($weight_kg * 2.2045)
-
+    
 # Convert kilograms to stone: kg * 0.157473 = stone 
 $GetStone = '{0:n2}' -f ($weight_kg * 0.157473)
-
-
+    
+    
 # Write a message to the user
 "Hello $name. Today is $date.`nYou weigh $GetPounds in pounds and you weigh $GetStone in stone."
 }
-
+    
 WeightMeasurements 
