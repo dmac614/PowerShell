004801fdb1b597c137202dd7a936b7167b18390a
dmac614 <89596346+dmac614@users.noreply.github.com>
2024-10-04 21:24:19 +1000

    Update Script idea - format teams conversation.ps1

--- Script idea - format teams conversation.ps1
+++ Script idea - format teams conversation.ps1
@@ -1,5 +1,37 @@
 <#
 
+# Teams conversation
+Charles Cook
+3:21 pm
+too many project behind the scene, alot of breakage.
+not enough people on the desk either by Daniel MacDonald
+3:21 pm
+Daniel MacDonald
+not enough people on the desk either
+1 Laugh reaction.
+1
+yeah John is the gate keeper by Charles Cook
+Charles Cook
+3:22 pm
+yeah John is the gate keeper
+now you know! by Charles Cook
+Charles Cook
+3:22 pm
+now you know!
+Can I call you about GPO when you are back by Daniel MacDonald
+3:46 pm
+Daniel MacDonald
+Can I call you about GPO when you are back
+yeah sure by Charles Cook
+Charles Cook
+4:06 pm
+yeah sure
+has context menu
+
+
+has context menu
+
+
 # Idea
 Get the content of the conversation which is stored in a file
 Get the name of the user and your name
