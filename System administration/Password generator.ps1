b28a0e1e2f6172b63f9914cb9f3dd367d9787809
dmac614 <89596346+dmac614@users.noreply.github.com>
2024-12-23 09:30:24 +1000

    Change variable names
    Changed empty array variable name to $password
    Changed the final variable name to $generatePassword

--- System administration/Password generator.ps1
+++ System administration/Password generator.ps1
@@ -1,3 +1,9 @@
+<# 
+
+This script works with Powershell 7 but not Powershell 5.1. 
+
+#>
+
 # Store all possible password characters
 $lowercaseArray = 'a'..'z'
 $uppercaseArray = 'A'..'Z'
@@ -6,16 +12,16 @@ $numbers = '0'..'9'
 $passwordLength = 16
 
 # Empty array to store the password in
-$generatePassword = @()
+$password = @()
 
 # Add data to the empty array 
-$generatePassword += $lowercaseArray | get-random -count 5
-$generatePassword += $uppercaseArray | get-random -count 5
-$generatePassword += $numbers | get-random -count 3
-$generatePassword += $symbolsArray | get-random -count 3
+$password += $lowercaseArray | get-random -count 5
+$password += $uppercaseArray | get-random -count 5
+$password += $numbers | get-random -count 3
+$password += $symbolsArray | get-random -count 3
 
 # Generate a random password
-$generatePassword = $generatePassword | Get-Random -count $generatePassword.Count
-$password = -join $generatePassword
+$password = $password | Get-Random -count $password.Count
+$generatePassword = -join $password
 
-$password
+$generatePassword
