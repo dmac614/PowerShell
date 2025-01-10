95431a23dee6ce2b8e29616edd812f24e5f417ec
dmac614 <89596346+dmac614@users.noreply.github.com>
2025-01-10 21:36:13 +1000

    add #region and #endregion comments

--- System administration/Get-ComputerInfo.ps1
+++ System administration/Get-ComputerInfo.ps1
@@ -18,17 +18,19 @@ function ComputerDetails {
     # Gather specs about the computer system
     Write-Output "Specifications about the computer system."
 
+    #region computer specs
     Get-ComputerInfo | 
     Select-Object csname,csusername,CsDomain,OsName,osversion,BiosSeralNumber,BiosSMBIOSBIOSVersion,CsManufacturer,CsModel,
     @{n='CsTotalPhysicalMemory';e={ '{0:N2}' -f ($_.CsTotalPhysicalMemory/1GB) }}
-
-    # Gather disk size and free space
+    #endregion computer specs
+    
+    #region disk space
     Write-Output "Disk information in gigabytes.`n"
     Get-CimInstance -ClassName Win32_LogicalDisk -Filter "drivetype=3" | 
     Select-object DeviceID,
     @{n='size';e={ '{0:N2}' -f ($_.Size / 1GB) }},
     @{n='freespace';e={ '{0:N2}' -f ($_.FreeSpace / 1GB) }}
-
+    #endregion disk space
 } # end function
 
 # Invoke the function
