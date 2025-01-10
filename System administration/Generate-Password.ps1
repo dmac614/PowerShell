d26badf30dfecdd4568f0ffcbab6d0c184e493d7
dmac614 <89596346+dmac614@users.noreply.github.com>
2025-01-10 21:38:43 +1000

    add #region and #endregion comments

--- System administration/Generate-Password.ps1
+++ System administration/Generate-Password.ps1
@@ -4,24 +4,28 @@ This script works with Powershell 7 but not Powershell 5.1.
 
 #>
 
-# Store all possible password characters
+
+#region all possible password characters
 $lowercaseArray = 'a'..'z'
 $uppercaseArray = 'A'..'Z'
 $symbolsArray = '/','?','$','[','%','!','.',';','+',']','{','#'
 $numbers = '0'..'9'
 $passwordLength = 16
+#endregion all possible password characters
 
+#region add characters to password array
 # Empty array to store the password in
 $password = @()
 
-# Add data to the empty array 
 $password += $lowercaseArray | get-random -count 5
 $password += $uppercaseArray | get-random -count 5
 $password += $numbers | get-random -count 3
 $password += $symbolsArray | get-random -count 3
+#endregion add characters to password array
 
-# Generate a random password
+#region generate a password
 $password = $password | Get-Random -count $password.Count
 $generatePassword = -join $password
 
 $generatePassword
+#endregion generate a password
