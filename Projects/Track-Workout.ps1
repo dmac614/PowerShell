aa43f9b514dbcec8998ed5a3b7a93c177fae76c2
dmac614 <89596346+dmac614@users.noreply.github.com>
2025-01-27 16:10:15 +1000

    Update Track-Workout.ps1

--- Projects/Track-Workout.ps1
+++ Projects/Track-Workout.ps1
@@ -22,71 +22,106 @@ param(
 
 
 #region Variables
-$FB1  = "Full Body 1"
-$FB2  = "Full Body 2"
+$FB1  = "Full Body #1"
+$FB2  = "Full Body #2"
 $Arms = "Arms"
 #endregion
 
-#region FullBody1 Class
-class FullBody1 {
-    #[datetime]$Date
-    [string]$Exercise
-    #[int]$Reps
-    [string]$FirstSet
-    [string]$SecondSet
-    #[int]$Weight
-    #[string]$Notes
+#region WorkoutTemplate class
+class WorkoutTemplate 
+{
+    # Properties
+    [string]$ExerciseName
+    [ValidateRange(1,150)]
+    [double]$Weight
+    [ValidateRange(1,30)]
+    [int]$Reps
+    [string]$Notes
 
-    FullBody1(
-    [string]$Exercise,
-    [string]$FirstSet,
-    [string]$SecondSet
-    ){
-        #$this.Date = $Date
-        $this.Exercise = $Exercise
-        #$this.Reps = $Reps
-        $this.FirstSet = $FirstSet
-        $this.SecondSet = $SecondSet
-        #$this.Weight = $Weight
-        #$this.Notes = $Notes
-        #Add these params to the FullBody1() constructor
+    # Methods
+    [void]ModifyWeight() { 
+        $this.Weight = [double](Read-Host -prompt "Enter the new weight")
+    }
+
+    [void]ModifyReps() {
+        $this.Reps = [int](Read-Host -prompt "Enter the new reps")
+      }
+
+    [WorkoutTemplate] AddSet(
+        [string]$name,
+        [double]$weight,
+        [int]$reps
+        ){
+          return [WorkoutTemplate]::new($name,$weight,$reps)
+    }
+
+    # Empty constructor
+    WorkoutTemplate(){}
+
+    # Primary Constructor
+    WorkoutTemplate(
+        [string]$ExerciseName,
+        [double]$Weight,
+        [int]$Reps
+        ){
+            $this.ExerciseName = $ExerciseName
+            $this.Weight = $Weight
+            $this.Reps = $Reps
     }
 }
 
-#FullBody1 new Objects
-$LyingLegCurl = [FullBody1]::new('Lying Leg Curl','8 reps @ 54KG','4 reps @ 54KG')
-$SeatedLegCurl = [FullBody1]::new('Seated Leg Curl','','')
-<# TODO: Add the remaining exercises as objects 
-$SeatedRow = [FullBody1]::new('Seated Row','','')
-$SeatedCableRow = [FullBody1]::new('Seated Cable Row','','')
-$InclineBench = [FullBody1]::new('Incline Bench Press','','')
-$InclineFly = [FullBody1]::new('Incline Fly (dumbbell)','','')
-$StandingCalf = [FullBody1]::new('Standing Calf Raise','','')
-#>
+#region WorkoutTemplate instance: FullBody1
+#$FullBody1 = @()
+$LyingLegCurl                   = [WorkoutTemplate]::new("Lying Leg Curl",53,8)
+$LyingLegCurl1                  = $LyingLegCurl.AddSet("Lying Leg Curl",53,6)
+
+$SeatedLegCurl                  = [WorkoutTemplate]::new("Seated Leg Curl",20,11)
+$SeatedLegCurl1                 = $SeatedLegCurl.AddSet("Seated Leg Curl",53,6)
+
+$SeatedRowCable                 = [WorkoutTemplate]::new("Seated Row Cable",31.8,10)
+$SeatedRowCable1                = $SeatedRowCable.AddSet("Seated Row Cable",31.8,10)
+
+$SeatedRowMachine_UpperBack     = [WorkoutTemplate]::new("Seated Row Machine (upper back)",65,6)
+$SeatedRowMachine_UpperBack1    = $SeatedRowMachine_UpperBack.AddSet("Seated Row Machine (upper back)",65,6)
+
+$InclineBenchPress              = [WorkoutTemplate]::new("Incline Bench Press",20,9)
+$InclineBenchPress1             = $InclineBenchPress.AddSet("Incline Bench Press",20,9)
+
+$InclineChestFly                = [WorkoutTemplate]::new("Incline Chest Fly",12.5,10)
+$InclineChestFly1               = $InclineChestFly.AddSet("Incline Chest Fly",12.5,10)
+
+$StandingCalves                 = [WorkoutTemplate]::new("Standing Calf Raise",18,10)
+$StandingCalves1                = $StandingCalves.AddSet("Standing Calf Raise",18,10)
+
+
+# FullBody1 workout
+$FullBody1 = $LyingLegCurl,$LyingLegCurl1,$SeatedLegCurl,$SeatedLegCurl1,$SeatedRowCable,$SeatedRowCable1,$SeatedRowMachine_UpperBack,$SeatedRowMachine_UpperBack1,$InclineBenchPress,$InclineBenchPress1,$InclineChestFly,$InclineChestFly1,$StandingCalves,$StandingCalves1
+# plan: make an empty array; add the results of the workout to the array; call the array
+
+$FullBody1
 #endregion
 
 
-#region DisplayWorkout
+#region Get-Workout
 function Get-Workout {
     param (
-        [array]$Workouts = @(
+            [array]$Workouts = @(
             Write-Host "`n1. $FB1" -ForegroundColor Blue
             Write-Host "2. $FB2" -ForegroundColor Green
             Write-Host "3. $Arms" -ForegroundColor White
             Write-Host "4. Exit`n" -ForegroundColor Red
         )
-    )
+    ) 
     # Display the workouts
     $Workouts
+
     # Get input to select a workout
     $WhichWorkout = Read-Host -prompt "Which workout did you perform today?"
     
     Switch ($WhichWorkout) {
         1 {
             Write-Host "`nYou chose: $($FB1)" -ForegroundColor Blue
-            $LyingLegCurl,$SeatedLegCurl
-            #,$SeatedRow,$SeatedCableRow,$InclineBench,$InclineFly,$StandingCalf 
-            # Find an easier way to write this
+            $FullBody1
         }
 
         2 {
@@ -115,4 +150,6 @@ Get-Workout
 
 #region UpdateWorkout
 # function Update-Workout { }
+#ModifyWeight() {}
+#ModifyReps() {}
 #endregion
