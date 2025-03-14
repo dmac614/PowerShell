95396c2601e4a66e6bb4dad7d9df7d2b357d4c2e
dmac614 <89596346+dmac614@users.noreply.github.com>
2025-03-14 09:46:28 +0000

    Complete update to script.ps1
    Contained code inside a function;
    Added conditional statements depending on the VM state; 
    Added try/catch blocks;

--- System administration/ConnectVM-Client02.ps1
+++ System administration/ConnectVM-Client02.ps1
@@ -12,24 +12,62 @@ ConnectVM-Client02.ps1
 Author: Daniel Macdonald
 
 #>
-[CmdletBinding()]
-param (
-    [Parameter(Mandatory=$False)][string]$vmName = "Client02-dmac (Win11)"
-)
+function Connect-VirtualMachine 
+{
+    [CmdletBinding()]
+    param 
+    (
+        [Parameter(Mandatory=$False)][string]$vmName = "Client02-dmac (Win11)"
+    )
 
-# Start the VM
-Write-Output "$vmName will now start"
-Start-VM -Name $vmName
+    # Start the VM
+    Write-Output "Attempting to start '$vmName'"
+    $GetVM = Get-VM -Name $vmName
 
-# Messages
-Write-Output "$vmName has started"
-Write-Output "Starting services. Wait 15s"
-Start-Sleep -Seconds 15
+    If ($GetVM.State -eq 'Off')
+    {
+        try 
+        {
+            # Start Virtual Machine
+            Start-VM -Name $vmName -WarningAction Stop
+            Write-Output "'$vmName' has started"
+            Write-Output "Starting VM services. Wait 10s"
+            Start-Sleep -Seconds 10
 
-# Open the VM Window
-Write-Output "Running $vmName process"
-C:\WINDOWS\system32\VmConnect.exe 'DMAC-PF3DE7NR' 'Client02-dmac (Win11)'
+            # Open the VM Window
+            Write-Output "Running '$vmName' process"
+            & ${env:WinDir}\system32\VmConnect.exe 'DMAC-PF3DE7NR' 'Client02-dmac (Win11)'
+        } 
+        catch 
+        {
+            Write-Output "Could not start '$vmName'"
+            Write-Output $_.Exception.Message
+        }
+    }
+    
+    ElseIf ($GetVM.State -eq 'Running')
+    {
+        try 
+        {
+            # the error is here: I need to check if the process window is open or not
+            $CheckVM = (Get-Process).MainWindowTitle.Contains("Client02-dmac (Win11) on DMAC-PF3DE7NR - Virtual Machine Connection")
+            if ($CheckVM)
+            {
+                Write-Output "VM window is already open"
+            }
+            else
+            {
+                Write-Output "Running '$vmName' process"
+                & ${env:WinDir}\system32\VmConnect.exe 'DMAC-PF3DE7NR' 'Client02-dmac (Win11)'
+            }
+        } 
+        catch 
+        {
+            Write-Output "Could not open '$vmName'"
+            Write-Output $_.Exception.Message
+        }
+    }
+}
 
-# Remote on to Client02 via PowerShell
-#$PSLab = enter-pssession -ComputerName $vmName -Credential AzureAD\Globaladmin
-#$PSLab
+# Call function
+Connect-VirtualMachine 
