5b2a8627fcdd901de4f5fef0701ce0180a18a14d
dmac614 <89596346+dmac614@users.noreply.github.com>
2025-04-22 21:53:08 +0100

    Create Call-Graph.ps1

---
+++ Microsoft Graph/Call-Graph.ps1
@@ -0,0 +1,27 @@
+## Unfinished ##
+
+# Modify the call to Microsoft Graph
+# More Data values will be added
+
+function Get-GraphAPI {
+param(
+    [Parameter(Mandatory=$true, Position=0)]
+    [ValidateSet("v1.0", "beta")]
+    [string]$Version,
+    [Parameter(Mandatory=$true, Position=1)]
+    [ValidateSet("users", "groups", "devicemanagement/manageddevices")]
+    [string]$Data
+    )   
+        # Auth to graph
+        Write-Output "Connecting to Graph..."
+        Start-Sleep -Seconds 2
+        Connect-MgGraph
+
+        # Pause for 10s
+        Write-Output "Pulling data in 10s..."
+        Start-Sleep -Seconds 10
+
+        # Get Graph data
+        $Objects = Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/$Version/$Data"
+        #$Objects.value # testing this
+}
