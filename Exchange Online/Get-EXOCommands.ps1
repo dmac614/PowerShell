0bb436763875114f080937f414f79ed1e37992d3
dmac614 <89596346+dmac614@users.noreply.github.com>
2025-07-06 22:05:36 +0100

    Create Get-EXOCommands.ps1

---
+++ Exchange Online/Get-EXOCommands.ps1
@@ -0,0 +1,24 @@
+<#
+.SYNOPSIS
+Retrieve EXO commands from the temporary module
+.DESCRIPTION
+The EXO v3 module has less cmdlets to use at first glance. Additional cmdlets are stored in local appdata and can be identified by searching for the commands of the module.
+.PARAMETER <parameter>
+No parameters are used for this script
+.EXAMPLE
+Get-EXOCommands
+
+.NOTES
+Author: Daniel Macdonald
+#>
+
+# Function to split the tmp module name and search for its commands
+function Get-EXOCommands {
+    $ModuleName = Get-ConnectionInformation | Select-Object ModuleName
+    $Split = $ModuleName.ModuleName.Split("$env:LOCALAPPDATA\Temp\")
+
+    Get-Command -Module $Split
+}
+
+# Evoke the function
+Get-EXOCommands
