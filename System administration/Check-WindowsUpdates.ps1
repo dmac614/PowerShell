fb2c569d9921f0f634ea1068e7c56f194f19ac5e
dmac614 <89596346+dmac614@users.noreply.github.com>
2025-07-10 23:38:59 +0100

    Updated script

---
+++ System administration/Check-WindowsUpdates.ps1
@@ -0,0 +1,38 @@
+﻿<#
+.SYNOPSIS
+Install Windows updates 
+
+.DESCRIPTION
+Install Windows updates 
+
+.NOTES
+Author: Daniel Macdonald
+
+-------------- unfinished --------------
+
+## Tasks ##
+Check for the installed module: PsWindowsUpdate
+If it's installed, continue/import the module
+Else, install the module
+
+#>
+    [Cmdletbinding()]
+    param ()
+
+    $UpdatesModule = Get-Module -Name PsWindowsUpdate 
+        try {
+            # Check for the module
+            if ($UpdatesModule) {
+                "The module $(($UpdatesModule).Name) is already installed. Importing the module:"
+                Import-Module -Name $UpdatesModule.Name -Verbose
+                "Module successfully imported"
+            } 
+            else {
+            "Installing the module $($UpdatesModule.Name)" 
+            Install-Module -Name $UpdatesModule.Name
+            }
+        } 
+        catch [System.Exception] {
+            "ERROR: Failed to install $($UpdatesModule.Name)"
+            "The fully qualified error ID is: $($_.FullyQualifiedErrorId)"
+        }
