b7f4ff7458ba6219a199fa2f8892b476729b6cbc
dmac614 <89596346+dmac614@users.noreply.github.com>
2025-07-11 18:53:54 +0100

    Added PSRepository region

--- System administration/Check-WindowsUpdates.ps1
+++ System administration/Check-WindowsUpdates.ps1
@@ -19,20 +19,51 @@ Else, install the module
     [Cmdletbinding()]
     param ()
 
-    $UpdatesModule = Get-Module -Name PsWindowsUpdate 
-        try {
-            # Check for the module
-            if ($UpdatesModule) {
-                "The module $(($UpdatesModule).Name) is already installed. Importing the module:"
-                Import-Module -Name $UpdatesModule.Name -Verbose
-                "Module successfully imported"
-            } 
-            else {
-                "Installing the module $($UpdatesModule.Name)" 
-                Install-Module -Name $UpdatesModule.Name
-            }
-        } 
-        catch [System.Exception] {
-            "ERROR: Failed to install $($UpdatesModule.Name)"
-            "The fully qualified error ID is: $($_.FullyQualifiedErrorId)"
+#region
+    <#  
+        Check the PowerShell repository
+        Trust PSGallery
+    #>
+    $PSGallery = Get-PSRepository
+    try {
+        if ($PSGallery.Name -eq "PSGallery" -and $PSGallery.InstallationPolicy -eq "Untrusted") {
+            Set-PSRepository -Name $PSGallery.Name -InstallationPolicy Trusted
+            Write-Output "Installation Policy is now set to 'Trusted'"
         }
+        else {
+            Write-Output "Installation Policy is set to 'Trusted'"
+        }
+    }
+    catch [System.Exception] {
+        Write-Error "Could not configure the PowerShell package management repository" 
+        Write-Error "The fully qualified error ID is: $($_.FullyQualifiedErrorId)" 
+        Write-Error "Error at line $($_.InvocationInfo.ScriptLineNumber): $($_.Exception.Message)" -ErrorAction Stop
+    }
+#endregion
+
+#region
+    <#
+        Check for the PsWindowsUpdate module
+    #>
+    $UpdatesModule = Get-Module -ListAvailable | ? { $_.name -match 'PsWindowsUpdate' }
+    try {
+        # If null -- install the module
+        if ([string]::IsNullOrEmpty($UpdatesModule)) {
+            Write-Output "Installing the module 'PsWindowsUpdate'" 
+            Install-Module -Name PsWindowsUpdate -Verbose
+            Write-Output "Module has successfully been installed"
+        } 
+        else {
+            Write-Output "The module $(($UpdatesModule).Name) is already installed. Importing the module:"
+            Import-Module -Name PsWindowsUpdate -Verbose
+            Write-Output "Module successfully imported"
+            }
+        }
+    catch [System.Exception] {
+        Write-Error "Failed to install 'PsWindowsUpdate'"
+        Write-Error "The fully qualified error ID is: $($_.FullyQualifiedErrorId)" 
+        Write-Error "Error at line $($_.InvocationInfo.ScriptLineNumber): $($_.Exception.Message)" -ErrorAction Stop
+        }
+#endregion
+
+
