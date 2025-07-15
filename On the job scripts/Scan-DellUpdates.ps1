3490446f26d047da93e8d12e7e9b937f55356eb4
dmac614 <89596346+dmac614@users.noreply.github.com>
2025-07-15 21:36:34 +0100

    Added param() block; added error handling

--- On the job scripts/Scan-DellUpdates.ps1
+++ On the job scripts/Scan-DellUpdates.ps1
@@ -12,65 +12,69 @@ https://www.dell.com/support/manuals/en-uk/command-update/dellcommandupdate_311_
 .NOTES
 Author: Daniel Macdonald
 
+Example of what the log file will say when updates are avilable
+<-- "Number of applicable updates for the current system configuration: 2" -->
+The first query will match if the log file contains 1+ update(s)
+The second query will match when the dcuApplyUpdate.log file installs the updates
 #>
+    param(
+        [string]$TextPattern = [regex]'Number of applicable updates .+ [1-9]{1,2}',
+        [string]$FinishedUpdates = 'Finished installing the updates.',
+        
+        # Necessary folder locations
+        [string]$64BitLocation = 'C:\Program Files\Dell\CommandUpdate',
+        [string]$32BitLocation = 'C:\Program Files (x86)\Dell\CommandUpdate',
+        [string]$TempFolder = 'C:\Temp'
+    )
+            # Navigate to dcu-cli location
+            if      (Test-Path -Path $64BitLocation) { Set-Location $64BitLocation } 
+            elseif  (Test-Path -Path $32BitLocation) { Set-Location $32BitLocation } 
+            else    { Write-Error "Dell Command | Update is not installed on this computer. " -ErrorAction Stop }
 
-# Example of what the log file will say when updates are avilable
-# <-- "Number of applicable updates for the current system configuration: 2" --> #
+            # Verify Temp folder exists
+            try {
+                if (!(Test-Path -Path $TempFolder)) {
+                    New-Item -Path ${env:SystemDrive}\ -Name Temp -ItemType Directory
+                    Write-Output "$TempFolder has been created"
+                } 
+                else {
+                    Write-Output "The $TempFolder directory for the log files already exists."
+                }
+            } 
+            catch {
+                Write-Error "Could not create $TempFolder folder"
+                Write-Error "The fully qualified error ID is: $($_.FullyQualifiedErrorId)" 
+                Write-Error "Error at line $($_.InvocationInfo.ScriptLineNumber): $($_.Exception.Message)" -ErrorAction Stop
+            }
 
-# The first query will match if the log file contains 1+ update(s)
-# The second query will match when the dcuApplyUpdate.log file installs the updates
-$TextPattern = [regex]'Number of applicable updates .+ [1-9]{1,2}'
-$FinishedUpdates = 'Finished installing the updates.'
+            Write-Output "Scanning the computer for updates..."
 
-# Dell Command Update folder locations
-$64BitLocation = 'C:\Program Files\Dell\CommandUpdate'
-$32BitLocation = 'C:\Program Files (x86)\Dell\CommandUpdate'
+            # Display app version and scan the computer for updates
+            .\dcu-cli.exe /version
+            .\dcu-cli.exe /scan -outputlog=C:\temp\dcuScan.log -silent
 
-# Navigate to dcu-cli location
-if (Test-Path -Path $64BitLocation) {
+            # Read the log file
+            $LogFile = Get-Content "$TempFolder\dcuScan.log"
+            $LogFile
 
-    # Change folder path
-    Set-Location $64BitLocation
+            # Apply available updates
+            if ($LogFile -match $TextPattern) {
 
-} elseif (Test-Path -Path $32BitLocation) {
+                # Message to the host
+                Write-Output "`nUpdates are available. Applying updates to the system..."
+                
+                # Apply updates
+                .\dcu-cli.exe /applyUpdates -outputlog=C:\temp\dcuApplyUpdate.log -silent -autoSuspendBitLocker=enable -reboot=disable
 
-    # Change folder path
-    Set-Location $32BitLocation
-
-} else {
-    Write-Warning "`nDell Command | Update is not installed on this computer. "
-}
-
-# Message to the host
-Write-Output "`nScanning the computer for updates...`n"
-
-# Display app version and scan the computer for updates
-.\dcu-cli.exe /version
-.\dcu-cli.exe /scan -outputlog=C:\temp\dcuScan.log -silent
-
-# Read the log file
-$LogFile = Get-Content C:\temp\dcuScan.log
-$LogFile
-
-# Apply available updates
-if ($LogFile -match $TextPattern) {
-
-    # Message to the host
-    Write-Output "`nUpdates are available. Applying updates to the system...`n"
-    
-    # Apply updates
-    .\dcu-cli.exe /applyUpdates -outputlog=C:\temp\dcuApplyUpdate.log -silent -autoSuspendBitLocker=enable -reboot=disable
-
-    # Get the content of the update log file
-    $UpdateFile = Get-content C:\temp\dcuApplyUpdate.log
-    $UpdateFile
-    
-    if ($UpdateFile -match $FinishedUpdates) {
-        # Write a message to the host if the dcuApplyUpdate.log file contains a regular expression 
-        Write-Output "`nUpdates have been installed successfully. `nConsider restarting your computer to complete the installation.`n"
-    }
-} 
-
-else {
-    Write-Output "`nNo updates are available.`nThis system is up to date."
-}
+                # Get the content of the update log file
+                $UpdateFile = Get-content "$TempFolder\dcuApplyUpdate.log"
+                $UpdateFile
+                
+                if ($UpdateFile -match $FinishedUpdates) {
+                    # Write a message to the host if the dcuApplyUpdate.log file contains a regular expression 
+                    Write-Output "`nUpdates have been installed successfully. `nConsider restarting your computer to complete the installation.`n"
+                }
+            } 
+            else {
+                Write-Output "`nNo updates are available.`nThis system is up to date."
+            }
