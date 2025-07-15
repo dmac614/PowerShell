170d56c4355b068766498640720ad34450d235a3
dmac614 <89596346+dmac614@users.noreply.github.com>
2025-07-15 23:13:08 +0100

    Update script to Get-TempFolder.ps1

---
+++ System administration/Get-TempFolder.ps1
@@ -0,0 +1,24 @@
+﻿<#
+.SYNOPSIS
+Creates the Temp folder if it doesn't exist
+
+.DESCRIPTION
+Creates the Temp folder if it doesn't exist
+
+.NOTES
+Author: Daniel Macdonald
+
+#>
+function Get-TempFolder {
+    param(
+        [string]$TempFolder = 'C:\Temp'
+    )       
+    # If the Temp folder doesn't exist, create it
+    if (! (Test-Path -Path $TempFolder)) {
+        New-Item -Path ${env:SystemDrive}\ -Name Temp -ItemType Directory
+        Write-Output "$TempFolder has been created"
+    } 
+    else {
+        Write-Output "$TempFolder already exists."
+    }
+}
