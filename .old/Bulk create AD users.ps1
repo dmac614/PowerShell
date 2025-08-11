4c5846772fe1f301d764fe642d3f7f6a77def63c
dmac614 <89596346+dmac614@users.noreply.github.com>
2025-08-11 21:16:50 +0100

    Added to .old

---
+++ .old/Bulk create AD users.ps1
@@ -0,0 +1,59 @@
+﻿<#
+.SYNOPSIS
+Creates multiple user accounts in Active Directory.
+
+.DESCRIPTION
+Creates multiple user accounts from corresponding user data csv file which stores the data into the hash table values. 
+
+.NOTES
+Author: Daniel Macdonald
+
+1. Add this code in later to check for duplicate accounts
+
+	# Check for a duplicate username i.e an existing account
+	if (Get-ADUser -Filter {SamAccountName -eq $row.SamAccountName}) {
+
+	# Write a warning if a username exists
+	Write-Warning -Message "An account with username $($Property.Username) already exists."
+	}
+
+	else { # Continue from (New-ADUser @UserParams) }
+
+
+2. Parameters to consider
+		-AccountExpirationDate
+
+#>
+
+# Import AD Module
+Import-Module -Name ActiveDirectory
+
+# NewUsers CSV File
+$CSVData = Import-CSV -Path "C:\PowerShellScripts\Bulk Create AD Users_data.csv"
+
+# ForEach to loop through each row in the CSV file
+foreach ($row in $CSVData) {
+
+	# User account parameters
+	$UserParams = @{
+
+	Name = $row.Name
+	GivenName = $row.FirstName
+	Surname = $row.Surname
+    	DisplayName = $row.Name
+	Email = $row.EmailAddress
+    	Company = $row.Company
+    	SamAccountName = $row.SamAccountName
+    	UserPrincipalName = $row.UserPrincipalName
+	Path = $row.Path
+    	AccountPassword = (ConvertTo-SecureString -AsPlainText $row.Password -Force)
+    	Enabled = $True 
+    	ChangePasswordAtLogon = $False 
+	}
+
+# Create new user accounts with the hashtable data
+New-ADUser @UserParams
+
+# When the user is created
+Write-Host "The user $($row.SamAccountName) is created." -ForegroundColor Green
+}
---
+++ .old/Measure-Weight.ps1
@@ -0,0 +1,36 @@
+﻿<#
+.SYNOPSIS
+Calculates my weight.
+
+.DESCRIPTION
+Uses a formula to calculate my weight in pounds, and in stone displaying both results as two decimal numbers.
+
+.NOTES
+Author: Daniel Macdonald
+
+#>
+
+function WeightMeasurements {
+# Variables
+$date = get-date
+    
+# Get user info
+[string]$Name = read-host -Prompt "What is your name?"
+[int]$weight_kg = read-host -Prompt "What is your weight in kilograms?"
+    
+# Convert kilograms to pounds: kg * 2.2045 = lbs
+$GetPounds = '{0:n2}' -f ($weight_kg * 2.2045)
+    
+# Convert kilograms to stone: kg * 0.157473 = stone 
+$GetStone = '{0:n2}' -f ($weight_kg * 0.157473)
+    
+    
+# Write a message to the user
+"Hello $name. Today is $date.`nYou weigh $GetPounds in pounds and you weigh $GetStone in stone."
+}
+    
+WeightMeasurements 
+
+
+
+## test changes ##
