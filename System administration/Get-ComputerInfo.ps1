284329e14520c35b0777ff882d8f9d4d2529b131
dmac614 <89596346+dmac614@users.noreply.github.com>
2025-08-13 21:33:49 +0100

    Placed the object properties in their respective arrays to achieve cleaner code

--- System administration/Get-ComputerInfo.ps1
+++ System administration/Get-ComputerInfo.ps1
@@ -14,23 +14,27 @@ Author: Daniel Macdonald
 
 # Function which contains the commands to gather the information
 function ComputerDetails {
+    param(
+        $CompInfoProps = @(
+            "csname","csusername","CsDomain","OsName","osversion","BiosSeralNumber","BiosSMBIOSBIOSVersion","CsManufacturer","CsModel",
+            @{n='CsTotalPhysicalMemory'; e={ '{0:N2}' -f ($_.CsTotalPhysicalMemory/1GB) }}
+        ),
 
-    # Gather specs about the computer system
-    Write-Output "Specifications about the computer system."
+        $DiskProps = @(
+            "DeviceID",
+            @{n='Size';e={ '{0:N2}' -f ($_.Size / 1GB) }},
+            @{n='FreeSpace';e={ '{0:N2}' -f ($_.FreeSpace / 1GB) }}
+        )
+    )
+        # Gather specs about the computer system
+        Write-Output "Specifications about the computer system:"
 
-    #region computer specs
-    Get-ComputerInfo | 
-    Select-Object csname,csusername,CsDomain,OsName,osversion,BiosSeralNumber,BiosSMBIOSBIOSVersion,CsManufacturer,CsModel,
-    @{n='CsTotalPhysicalMemory';e={ '{0:N2}' -f ($_.CsTotalPhysicalMemory/1GB) }}
-    #endregion computer specs
-    
-    #region disk space
-    Write-Output "Disk information in gigabytes.`n"
-    Get-CimInstance -ClassName Win32_LogicalDisk -Filter "drivetype=3" | 
-    Select-object DeviceID,
-    @{n='Size';e={ '{0:N2}' -f ($_.Size / 1GB) }},
-    @{n='FreeSpace';e={ '{0:N2}' -f ($_.FreeSpace / 1GB) }}
-    #endregion disk space
+        # Computer specs
+        Get-ComputerInfo | Select-Object $CompInfoProps
+        
+        # Disk space
+        Write-Output "Disk information in gigabytes:`n"
+        Get-CimInstance -ClassName Win32_LogicalDisk -Filter "drivetype=3" | Select-object $DiskProps
 } # end function
 
 # Invoke the function
