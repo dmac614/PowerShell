a5d09fdd62bab41dbca79701fd26f7d46b2f1e44
dmac614 <danielmacdonald-1@hotmail.com>
2025-09-17 17:57:07 +0100

    add

--- Projects/Nutrition Plan/NutritionPlan-Simple.ps1
+++ Projects/Nutrition Plan/NutritionPlan-Simple.ps1
@@ -1,9 +1,6 @@
 <#
     Aim: multiply the values of items within a hashtable by the value of the respective "Day" variable
 #>
-
-
-## Variables
 param(
     [int]$GymDays = 3,
     [int]$HighDays = 3,
@@ -19,52 +16,41 @@ param(
     }
 
     # Snack (array)
-
     $snack1 = @(
-        Egg = 2
-        Feta = 20
-        Dates = 3
-        Butter = 8
+        "Egg","Feta","Dates","Butter"
     )
 
+    function CalculateSnack {
+        $Snack * $GymDays
+    }
+
+    CalculateSnack
 
 
 
-    # Meal 1
-        $MealOne = @{
-            "Yogurt 0%" = 400
-            "Frozen Berries" = 100
-            Banana = 1
-        }
-
-    # Meal 2
-        $MealTwo = @{
-            "Ground Beef" = 200
-            Feta = 30
-            "Bell pepper" = 1
-            Courgette = 0.5
-            Passata = 125
-        }
-
-    # Meal 3
-        $MealThree = @{
-            Salmon = 200
-            "Avocado (small)" = 1
-            "Bell pepper" = 1
-            Courgette = 0.5
-            Passata = 125
-        }
-
-        function CalculateSnack {
-            $Snack * $GymDays
-        }
-
-        CalculateSnack
 
 
+    # # Meal 1
+    #     $MealOne = @{
+    #         "Yogurt 0%" = 400
+    #         "Frozen Berries" = 100
+    #         Banana = 1
+    #     }
 
-Write-Output $item
-Write-Output $Snack.Keys
-Write-Output $snack.Values
+    # # Meal 2
+    #     $MealTwo = @{
+    #         "Ground Beef" = 200
+    #         Feta = 30
+    #         "Bell pepper" = 1
+    #         Courgette = 0.5
+    #         Passata = 125
+    #     }
 
-$snack.
+    # # Meal 3
+    #     $MealThree = @{
+    #         Salmon = 200
+    #         "Avocado (small)" = 1
+    #         "Bell pepper" = 1
+    #         Courgette = 0.5
+    #         Passata = 125
+    #     }
