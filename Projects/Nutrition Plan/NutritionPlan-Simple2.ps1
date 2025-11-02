3a6e270500ec832569f687c932f1939479c0f27f
dmac614 <danielmacdonald-1@hotmail.com>
2025-11-02 17:02:03 +0000

    Rework attempt

---
+++ Projects/Nutrition Plan/NutritionPlan-Simple2.ps1
@@ -0,0 +1,11 @@
+<#
+# Plan #
+
+To have a list of food items as options
+To be able to enter the weight/quantity of the food items
+Calculate days (consuming food items) by the weight/quantity
+
+To combine the food items to make a meal with the items
+#>
+
+
