f92f32a35868ff739cb480fcafd849fc46765283
dmac614 <danielmacdonald-1@hotmail.com>
2025-11-07 09:22:58 +0000

    Automated the full process: from downloading the installer file

--- Intune/ConvertTo-IntuneWin.ps1
+++ Intune/ConvertTo-IntuneWin.ps1
@@ -19,21 +19,19 @@ This is the folder for .intunewin files
 Author: Daniel Macdonald
 
 #>
-[CmdletBinding()]
-param(
-    [Parameter(Mandatory)][string]$AppExe
-)
-
 # Variables 
 [string]$IntuneUtil = "C:\MicrosftWin32ContentPrepTool\IntuneWinAppUtil.exe"
 [string]$Source = "C:\MicrosftWin32ContentPrepTool\Apps"
 [string]$Output = "C:\MicrosftWin32ContentPrepTool\Intune files"
 
+# The downloaded item
+$DownloadedFileName = (get-childitem ${env:USERPROFILE}\downloads | sort LastWriteTime)[-1].FullName | Split-Path -Leaf
+
 # Move item from Downloads
-Move-Item "${env:USERPROFILE}\Downloads\$AppExe" C:\MicrosftWin32ContentPrepTool\Apps\ -Verbose
+Move-Item "${env:USERPROFILE}\Downloads\$DownloadedFileName" C:\MicrosftWin32ContentPrepTool\Apps\ -Verbose
 
 # Convert to .intunewin file
-& $IntuneUtil -c $Source -s $AppExe -o $Output
+& $IntuneUtil -c $Source -s $DownloadedFileName -o $Output
 
 # Open th directory
 Write-Output "Opening the '$Output' directory"
