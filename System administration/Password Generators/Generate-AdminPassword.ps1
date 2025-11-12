53a902e26e42adc93e2623b06f3e3c51b6ac2db7
dmac614 <danielmacdonald-1@hotmail.com>
2025-11-12 16:39:08 +0000

    Fix typo

--- System administration/Password Generators/Generate-AdminPassword.ps1
+++ System administration/Password Generators/Generate-AdminPassword.ps1
@@ -4,9 +4,9 @@ This script works with Powershell core but not Windows Powershell.
 #region Generate a password based on account type
 function GenerateAdminPassword {
 
-    $lowercaseArray = 'a'..'z',
-    $uppercaseArray = 'A'..'Z',
-    $symbolsArray = @('/','?','$','[','%','!','.',';','+',']','{','#'),
+    $lowercaseArray = 'a'..'z'
+    $uppercaseArray = 'A'..'Z'
+    $symbolsArray = @('/','?','$','[','%','!','.',';','+',']','{','#')
     $numbers = '0'..'9'
 
     # Empty array to store the User Account password
