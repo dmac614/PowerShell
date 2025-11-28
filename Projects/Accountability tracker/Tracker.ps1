d0afe0e37b88eb797c86437741e96aaf38d14adb
dmac614 <danielmacdonald-1@hotmail.com>
2025-11-28 21:18:14 +0000

    Updated the 'user input' section and finally got this project working. Added data to my first tracked file -- 28 Nov 2025

--- Projects/Accountability tracker/Tracker.ps1
+++ Projects/Accountability tracker/Tracker.ps1
@@ -39,13 +39,13 @@ $Path = "C:\PowerShell Dev\PowerShell\Projects\Accountability tracker\Tracker Da
 (!(Test-Path $Path\$folderName)) ? 
         (Write-Output "New folder created: $folderName"),
         (New-Item -ItemType Directory -Name $folderName -Path $Path | Out-Null) 
-    : "$Path\$folderName"
+    : "Monthly folder exists:   $Path\$folderName"
 
 
 (!(Test-Path $Path\$folderName\$fileName)) ? 
         (Write-Output "New file created: $fileName"),
         (New-Item -ItemType File -Path $Path\$folderName -Name $fileName | Out-Null) 
-    : "$Path\$folderName\$fileName"
+    : "Daily file exists:       $Path\$folderName\$fileName"
 
 #endregion
 
@@ -89,229 +89,73 @@ GetDays
 ####################################
 
 
-#region User input 
+if ((Get-Date).DayOfWeek -in $dayGym.DayOfWeek){ 
 
-$gymDayVariables = [pscustomobject]@{
-    "Nutrition Plan"    = $q1
-    "7K steps"          = $q2
-    "1L water"          = $q3
-    "Gym session"       = $q4
-    #"Notes"             = $dailyNote
+    "Daily habits"
+    "`nHint: respond with 'Yes' or 'No'"
+    [ValidateSet("Yes","No")]$q1 = Read-Host -Prompt "Did you follow the nutrition plan today?" 
+    [ValidateSet("Yes","No")]$q2 = Read-Host -Prompt "Did you complete 7k steps today?"
+    [ValidateSet("Yes","No")]$q3 = Read-Host -Prompt "Did you drink 1L of water with added salt today?"
+    [ValidateSet("Yes","No")]$q4 = Read-Host -Prompt "Did you complete the gym session today?"
 
-}
+        $qht = [ordered]@{
+            Q1 = $q1
+            Q2 = $q2
+            Q3 = $q3
+            Q4 = $q4
+        }
 
-$otherDayVariables = [pscustomobject]@{
-    "Nutrition Plan"    = $q1
-    "7K steps"          = $q2
-    "1L water"          = $q3
-    #"Notes"             = $dailyNote
-}
+    $gymDayVariables = [ordered]@{
+        "Nutrition Plan"    = $qht.Values[0]
+        "7K steps"          = $qht.Values[1]
+        "1L water"          = $qht.Values[2]
+        "Gym session"       = $qht.Values[3]
+    }
 
-if ((Get-Date).DayOfWeek -match $dayGym){ 
-    $gymDayVariables
-     
-} elseif ((Get-Date).DayOfWeek -match $dayMonth) {
+    $dailyNote = Read-Host "`nEnter notes for today"
+
     
-    #display the results $otherDayVariables
-    $otherDayVariables
+    # append data to the daily file
+    "`nAdded data to: $fileName"
+    $gymDayVariables | Out-File -FilePath "$Path\$folderName\$fileName"
+    Write-Output "Daily note: $dailyNote" | Out-File -FilePath "$Path\$folderName\$fileName" -Append
+
+    Get-Content "$Path\$folderName\$fileName"
+
+
+} elseif ((Get-Date).DayOfWeek -in $dayMonth.DayOfWeek) {
+    
+    "Daily habits"
+    "`nHint: respond with 'Yes' or 'No'"
+    [ValidateSet("Yes","No")]$q1 = Read-Host -Prompt "Did you follow the nutrition plan today?" 
+    [ValidateSet("Yes","No")]$q2 = Read-Host -Prompt "Did you complete 7k steps today?"
+    [ValidateSet("Yes","No")]$q3 = Read-Host -Prompt "Did you drink 1L of water with added salt today?"
+    
+        $qht = [ordered]@{
+            Q1 = $q1
+            Q2 = $q2
+            Q3 = $q3
+        }
+
+    $otherDayVariables = [ordered]@{
+        "Nutrition Plan"    = $qht.Values[0]
+        "7K steps"          = $qht.Values[1]
+        "1L water"          = $qht.Values[2]
+        #"Notes"             = $dailyNote
+    }
+
+    $dailyNote = Read-Host "`nEnter notes for today"
+
+
+    # append data to the daily file
+    "`nAdded data to: $fileName"
+    $otherDayVariables | Out-File -FilePath "$Path\$folderName\$fileName"
+    Write-Output "Daily note: $dailyNote" | Out-File -FilePath "$Path\$folderName\$fileName" -Append
+
+    Get-Content "$Path\$folderName\$fileName"
+
 
 } else { Write-Error "User input failed" }
 
 
 
-#$dailyNote = Read-Host "Enter notes for today"
-
-
-# function GetAnswers() {
-# <#
-#     Ask the Qs first
-#     Their answers will be stored in the pscustomobject
-# #>
-#     param(
-#         [ValidateSet("A","Y","N")]
-#         $Answer
-#     )
-#     $q1 = Read-Host -Prompt "Did you follow the nutrition plan today?" 
-#     $q2 = Read-Host -Prompt "Did you complete 7k steps today?"
-#     $q3 = Read-Host -Prompt "Did you drink 1L of water with added salt today?"
-#     $q4 = Read-Host -Prompt "Did you complete the gym session today?"
-
-
-# }
-
-# GetAnswers
-#endregion
-
-
-############################################################
-
-#region testing stuff
-<#
-
-
-$DailyVariables = @( 
-    "Follow nutrition plan",
-    "Drink 3L water",
-    "Take vitamins",
-    "Follow sleep pattern",
-    "Workout",
-    "Check-in"
-    )
-
-
-# Change all completed variables to Yes
-#function AllCompleted() {}
-
-function CreateResults() {
-    param(
-        [ValidateSet("Yes", "No")]
-        [string]$AllCompleted
-    )
-    
-    #($weekly in $WeeklyVariables)
-    #"Weekly variables"      = $weekly
-    
-
-    $completed  = $AllCompleted
-    $notes      = "Notes"
-    $Results    = @()
-
-    foreach ($daily in $DailyVariables) {
-
-            $dailyObjects = [pscustomobject]@{  
-                "Daily variables"       = $daily
-                "Completed"             = if ($daily -eq $DailyVariables[4] -or $daily -eq $DailyVariables[5]) {$null} else {$AllCompleted}
-                "Notes"                 = $notes
-        }
-    
-    $Results += $dailyObjects
-                    
-    }
-
-
-    $Results
-
-} #endfunction
-
-# Call function
-CreateResults -AllCompleted Yes
-
-
-
-
-
-
-
-
-
-
-    Testing
-
-    # Lists all the days in the month for 2025 and 2026
-    $months = 1..12
-    $daysInMonth = [system.datetime]::DaysInMonth
-    foreach ($m in $months) { $daysInMonth.Invoke(2025,$m) }
-    foreach ($m in $months) { $daysInMonth.Invoke(2026,$m) }
-
-    $dateNow = [system.datetime]::now
-
-
-#>
-
-
-#     $Nov = 11
-#     $daysInMonth = [system.datetime]::DaysInMonth
-#     $DateFormat = @('DayOfWeek','Day','Month','Year')
-#     foreach ($m in $Nov) { }
-#         $daysInMonth.Invoke(2025,$Nov)
-        
-
-#     #region List the days for one week
-#     $Nov = 11
-#     $DateFormat = @('DayOfWeek','Day','Month','Year')
-#     $d = [system.datetime]::DaysInMonth(2025,$Nov)
-#             for ($i = 1; $i -le 7; $i++) {
-#                 Get-Date -Month $Nov -Day $i | Select-Object $DateFormat
-#             }
-#     #endregion
-
-
-#     #region List the days of a single month
-    # $Nov = 11
-    # $DateFormat = @('DayOfWeek','Day','Month','Year')
-    # $GymDays = @('Tuesday', 'Friday', 'Saturday')
-    # $d = [system.datetime]::DaysInMonth(2025,$Nov)
-    #         for ($i = 1; $i -le $d; $i++) {
-    #             Get-Date -Month $Nov -Day $i | ? {$_.DayOfWeek -in $GymDays}  | Select-Object $DateFormat
-    #         }
-#     #endregion
-            
-
-#     #region List the days of the entire year
-
-#     #$daysInMonth.Invoke(2025,$Nov)
-#     #$daysInMonth = [system.datetime]::DaysInMonth(2025,$m)
-    
-#     $Months = 1..12
-#     $DateFormat = @('DayOfWeek','Day','Month','Year')
-#     foreach ($m in $Months) { 
-#         $d = [system.datetime]::DaysInMonth(2025,$m)
-#             for ($i = 1; $i -le $d; $i++) {
-#                 Get-Date -Month $m -Day $i | Select-Object $DateFormat
-#             }
-#     }
-#     #endregion
-            
-
-
-
-#     # Display the calendar month option
-#     $Nov = 11
-#     $DateFormat = @('DayOfWeek','Day')
-#     $Display = Get-Date
-    
-#     function DisplayDate {
-        
-#         $d = [system.datetime]::DaysInMonth(2025,$Nov)
-#         $Display.GetDateTimeFormats()[-1]
-#         for ($i = 1; $i -le $d; $i++) {
-#             Get-Date -Month $Nov -Day $i | 
-#             % ($_.DayOfWeek) { 
-#                 $CustomDays = [pscustomobject]@{
-#                     Sunday = $i[0]
-#                     Monday = $i[0]
-#                     Tuesday = $i[2]
-#                     Wednesday = $i[3]
-#                     Thursday = $i[4]
-#                     Friday = $i[5]
-#                     Saturday = $i[6]
-#                 }
-#                 Write-Output $CustomDays | ft
-#         }
-#     }
-# } DisplayDate
-
-
-# $Table = [pscustomobject]@{
-#     "Follow nutrition plan" = ""
-#     "Drink 3L water" = ""
-#     "Take vitamins" = ""
-#     "Workout" = ""
-#     "Check-in" = ""
-#     "Follow sleep pattern" = ""
-# }
-
-#     function DisplayDate-Week {
-
-#     $Nov = 11
-#     $Display = Get-Date
-#     Write-Output "Week 1 - $($Display.GetDateTimeFormats()[-1])"
-#     $DateFormat = @('DayOfWeek','Day')
-
-#     $d = [system.datetime]::DaysInMonth(2025,$Nov)
-#             for ($i = 1; $i -le 7; $i++) {
-#                 Get-Date -Month $Nov -Day $i | Select-Object $DateFormat
-#             }
-    
-#     }   DisplayDate-Week
-#endregion
