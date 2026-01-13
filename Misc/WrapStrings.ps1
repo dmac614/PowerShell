08ecad9b0776ca2703f49936aa267f9bb695c404
dmac614 <danielmacdonald-1@hotmail.com>
2026-01-13 13:19:33 +0000

    Create script

---
+++ Misc/WrapStrings.ps1
@@ -0,0 +1,17 @@
+# This method ensures the last item does not end with a comma
+$Strings = Get-Content -Path .\IDs.txt
+
+function WrapStrings {
+    for ($i = 0; $i -lt $Strings.Count; $i++) {
+        if ($i -eq $Strings.Count - 1) {
+            # Last item, no comma
+            '"' + $Strings[$i] + '"'
+        }
+        else {
+            '"' + $Strings[$i] + '",'
+        }
+    }
+}
+
+# Copy strings to clipboard
+WrapStrings | clip
