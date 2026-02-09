d146312d7bd1edae84ae430ad92ec6c7ebe26a89
dmac614 <danielmacdonald-1@hotmail.com>
2026-02-09 17:44:11 +0000

    update path syntax

--- Misc/WrapStrings.ps1
+++ Misc/WrapStrings.ps1
@@ -1,5 +1,5 @@
 # This method ensures the last item does not end with a comma
-$Strings = Get-Content -Path .\IDs.txt
+$Strings = Get-Content -Path 'C:\PowerShell Dev\PowerShell\Misc\IDs.txt'
 
 function WrapStrings {
     for ($i = 0; $i -lt $Strings.Count; $i++) {
