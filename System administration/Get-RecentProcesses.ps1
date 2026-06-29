031a4db1adb0182d5184e65b767cd419df235c71
dmac614 <danielmacdonald-1@hotmail.com>
2026-06-29 15:47:39 +0100

    Updated this to get the first 20 processes

--- System administration/Get-RecentProcesses.ps1
+++ System administration/Get-RecentProcesses.ps1
@@ -1,4 +1,4 @@
 # Get processes started in the last 10 minutes
 Get-Process | 
 ? { $_.StartTime -ge (get-date).AddHours(-0.2) } | 
-Select-object ProcessName,StartTime | Sort-Object StartTime -Descending
+Select-object ProcessName,StartTime -First 20 | Sort-Object StartTime -Descending
